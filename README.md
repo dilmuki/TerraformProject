c0ba3b754f9d416355d2e3d1f7f19aa0af22c908
dilmurod <dilmuki@gmail.com>
2018-10-28 21:56:28 +0000

    working

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # TerraformProject
-ProjectTerrafom
+keys.tf is ignored. Please create keys.tf with public key in it.
---
+++ output.tf
@@ -0,0 +1,8 @@
+output "public_ip" {
+  value = "${aws_instance.instance1.public_ip}"
+}
+output "ID" {
+  value = "${aws_instance.instance1.id}"
+}
+
+
