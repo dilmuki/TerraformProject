0259f5096be7ba135e0ea62a4ef59f68f7e9f11b
dilmurod <dilmuki@gmail.com>
2018-11-01 02:35:06 +0000

    Jenkins Keys added

--- keys.tf
+++ keys.tf
@@ -3,3 +3,7 @@ resource "aws_key_pair" "project1" {
   public_key = "${file("id_rsa.pub")}"
 }
 
+resource "aws_key_pair" "jenkins" {
+	key_name = "jenkins"
+	public_key = "/var/lib/jenkins/.ssh/id_rsa.pub"
+}
